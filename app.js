f2483f397a101646643072027324ae7bad2fd19c
elboj <adaja.tobi1@gmail.com>
2024-10-14 11:30:04 +0100

    added connection string'

--- app.js
+++ app.js
@@ -15,12 +15,9 @@ app.use(cors())
 //     user: process.env.MONGO_USERNAME,
 //     pass: process.env.MONGO_PASSWORD,
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    // 'mongodb+srv://supercluster.d83jj.mongodb.net/superData'
-    // user: 'superuser',
-    // pass: 'SuperPassword',
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', { 
+    user: 'superuser',
+    pass: 'SuperPassword',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
